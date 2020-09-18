ce47338da763b2c3093fed17d89e62ad562cbda1
Precious Ogoru <preciousexcept@gmail.com>
2020-09-18 13:16:45 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+//navigation
+const text = document.querySelector(".down");
+const move = document.querySelector(".sow")
+
+text.addEventListener("click", function () {
+  move.classList.toggle('sow');
+
+});
+
+document.querySelector('.main-btn').addEventListener('click', () => document.querySelector('.mobile-nav')
+  .classList.toggle('show'));
+
+document.querySelector('.dropdown').addEventListener('click', () => document.querySelector('.shown')
+  .classList.toggle('open'));
+
+//sliders
+$('.single-item').slick({
+  autoplay: true,
+  infinite: true,
+  autoplaySpeed: 5000,
+  nextArrow: ('.next'),
+  prevArrow: ('.prev'),
+});
+
+$('.p-slick').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        autoplay: true,
+        infinite: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+//animate on scroll
+
+
+// Initi AOS
+function aos_init() {
+  AOS.init({
+    duration: 1000,
+    once: true
+  });
+}
+$(window).on('load', function () {
+  aos_init();
+});
+
+(jQuery);
